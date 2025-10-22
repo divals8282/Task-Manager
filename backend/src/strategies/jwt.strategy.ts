e092bb0862ab78de59841482a7cd4ffb13c1dd8e
Hovhannes <hovhannes.amirjanyanit@gmail.com>
2025-10-22 15:13:14 +0400

    implemented jwt,local strategies

---
+++ backend/src/strategies/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../modules/auth/auth.service';
+import { JwtPayloadI } from '../types';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET as string,
+    });
+  }
+
+  async validate(payload: JwtPayloadI) {
+    return await this.authService.getUserInfo(payload.id);
+  }
+}
